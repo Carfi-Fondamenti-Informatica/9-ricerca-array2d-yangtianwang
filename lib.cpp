6c1b46c2678a653527d6ef1d3257f56591da9965
zhuoxiao yang <91831716+yangtianwang@users.noreply.github.com>
2021-12-15 21:04:42 +0800

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
 
+#include "lib.h"
+#include <cstring>
+
+bool ricerca(char lista [10][20], char nome[1][20], int &pos){
+	for (int i=0; i<10; i++){
+		if (strcmp (lista [i], nome [0])==0){
+			pos = i;
+			return true;
+			
+			}
+
+	}
+	return false;
+}
