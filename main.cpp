60f58560d8579ccfda6da05f3e9f461bedf6b7a4
zhuoxiao yang <91831716+yangtianwang@users.noreply.github.com>
2021-12-15 21:03:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+	int pos;
+	char a[10][20], f[1][20];
+	
+	for (int i=0; i<10; i++){
+		cin >> a[i];
+	}
+	cin >> f[0];
+	
+	if (ricerca(a, f, pos)){
+		cout << pos << endl;
+	}else{
+	    cout << "non presente" << endl;
+	}
+	return 0;
+	
 }
